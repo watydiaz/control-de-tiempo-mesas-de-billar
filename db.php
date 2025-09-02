137949f657c028f739c9f53076b50c6bc39f2f9b
watydiaz <karol.jesusdiaz@gmail.com>
2025-09-01 21:24:23 -0500

    Se independizan css

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "billar";
+$db = "terkkos";
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
---
+++ views/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Control de Mesas de Billar</title>
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#fbff14">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-title" content="Terkko's Billar">
+    <link rel="apple-touch-icon" href="img/logo.jpg">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="msapplication-TileImage" content="img/logo.jpg">
+    <meta name="msapplication-TileColor" content="#fbff14">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/main.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <header>
+        <div class="header-logo">
+            <img src="img/logo.jpg" alt="Logo" style="height:140px; width:auto; display:block; background:none; border:none;">
+        </div>
+        <div class="header-title">
+            Terkko's Billiard's Club
+        </div>
+        <nav class="menu-principal">
+            <a href="index.php">Inicio</a>
+            <a href="#mesasSlider">Mesas</a>
+            <a href="#ventaTotalDia">Venta Total</a>
+            <a href="#">Contacto</a>
+        </nav>
+    </header>
